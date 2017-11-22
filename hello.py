8e6aeb363f09803ee5ff3eaef4168a5518d39e5c
JasperChaoYu <31150278+JasperChaoYu@users.noreply.github.com>
2017-11-22 11:14:41 +0800

    hello

---
+++ hello.py
@@ -0,0 +1,14 @@
+## Testing for github using python code
+
+class HelloWorld:
+    def __init__(self):
+        self.__version = 1.0
+        self.__words = ""
+
+    @property
+    def Print:
+        return self.__words
+
+    def say_hello(self, words):
+        self.__words = words
+        print(words)
